bb9237a5b4d285328f8bd6753d675f817500ad11
esdes-kanyinda-keven <32100363+esdes-kanyinda-keven@users.noreply.github.com>
2018-07-05 10:55:39 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+console.log(document);
+
+var name="Keven";
+
+function changeName(newName) {
+   name=newName;
+   alert(name);
+}
+
+function changeClass () {
+   var button=document.getElementById("myButton");
+   if (button.className=="rotate") {
+       button.className="";
+
+   } else {
+       button.className="rotate";
+   }
+
+}
+
+anime ({
+    targets: '#coming-soon',
+    scale: 1.2,
+    opacity: 1,
+    delay: 1000,
+    
+})
